8be783c75e8027445295036b20cf15f7bc39a807
Lucie Gernidos <lgernido@bess-f3r1s9.clusters.42paris.fr>
2024-05-12 11:39:40 +0200

    colors are done

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: lgernido <lgernido@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 08:49:40 by lgernido          #+#    #+#             */
-/*   Updated: 2024/05/12 09:46:27 by lgernido         ###   ########.fr       */
+/*   Updated: 2024/05/12 11:39:08 by lgernido         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,8 @@ Zombie::Zombie(std::string const name)
 
 Zombie::~Zombie()
 {
-    std::cout << RED << this->get_name() << " is dead" << RESET << std::endl;
+    std::cout << RED << BOLD << this->get_name() << \
+    RESET << RED << " is dead" << RESET << std::endl;
     return ;
 }
 
@@ -37,5 +38,6 @@ std::string Zombie::get_name(void)
 
 void Zombie::announce(void)
 {
-    std::cout << GREEN << this->get_name() << ": BraiiiiiiinnnzzzZ..." << RESET << std::endl;
+    std::cout << GREEN << BOLD << this->get_name() << \
+    RESET << GREEN << ": BraiiiiiiinnnzzzZ..." << RESET << std::endl;
 }
--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: lgernido <lgernido@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 08:49:33 by lgernido          #+#    #+#             */
-/*   Updated: 2024/05/12 09:45:42 by lgernido         ###   ########.fr       */
+/*   Updated: 2024/05/12 11:37:51 by lgernido         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,15 @@
 #define RED		"\033[0;31m"
 #define GREEN	"\033[0;32m"
 #define YELLOW	"\033[0;33m"
+#define ORANGE "\033[38;5;208m"
 #define BLUE	"\033[0;34m"
 #define MAGENTA	"\033[0;35m"
 #define CYAN	"\033[0;36m"
 #define WHITE	"\033[0;37m"
-#define RESET	"\033[0m"
-
+#define BOLD       "\033[1m"
+#define UNDERLINE  "\033[4m"
+#define ITALIC     "\033[3m"
+#define RESET      "\033[0m"
 class Zombie
 {
     public :
