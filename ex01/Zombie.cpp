06cca0792081529009747a66659c39ddf8f4a7e9
Lucie Gernidos <lgernido@bess-f3r1s9.clusters.42paris.fr>
2024-05-12 09:45:15 +0200

    add color and fix ex01

--- ex01/Zombie.cpp
+++ ex01/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: lgernido <lgernido@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 08:49:40 by lgernido          #+#    #+#             */
-/*   Updated: 2024/05/10 13:15:53 by lgernido         ###   ########.fr       */
+/*   Updated: 2024/05/12 09:44:19 by lgernido         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ Zombie::Zombie(std::string const name)
 
 Zombie::~Zombie()
 {
-    std::cout << this->get_name() << " is dead" << std::endl;
+    std::cout << RED << this->get_name() << " is dead" << RESET << std::endl;
     return ;
 }
 
@@ -41,5 +41,5 @@ void Zombie::set_name(std::string name)
 
 void Zombie::announce(void)
 {
-    std::cout << this->get_name() << ": BraiiiiiiinnnzzzZ..." << std::endl;
+    std::cout << GREEN << this->get_name() << ": BraiiiiiiinnnzzzZ..." << RESET << std::endl;
 }
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lgernido <lgernido@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 12:01:50 by lgernido          #+#    #+#             */
-/*   Updated: 2024/05/10 12:30:47 by lgernido         ###   ########.fr       */
+/*   Updated: 2024/05/12 09:35:27 by lgernido         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,26 @@ Zombie *zombieHorde(int n, std::string name);
 
 int main(void)
 {
-    Zombie *Horde = NULL;
-    Horde = zombieHorde(5, "Michael");
-    for (int i = 0; i < 5; i++)
-        Horde->announce();
-    delete[] Horde;
+    {
+        Zombie *Horde = NULL;
+        Horde = zombieHorde(5, "Michael");
+        for (int i = 0; i < 5; i++)
+            Horde[i].announce();
+        delete[] Horde;
+    }
+    {
+        Zombie *Horde = NULL;
+        Horde = zombieHorde(1, "Kobe");
+        for (int i = 0; i < 1; i++)
+            Horde[i].announce();
+        delete[] Horde;
+    }
+    // {
+    //     Zombie *Horde = NULL;
+    //     Horde = zombieHorde(100, "Lebron");
+    //     for (int i = 0; i < 100; i++)
+    //         Horde[i].announce();
+    //     delete[] Horde;
+    // }
     return (0);
 }
